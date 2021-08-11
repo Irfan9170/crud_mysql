ca0bc1708091b4ca1d662ca62025585d82b65a44
Irfan <irfanali987654@gmail.com>
2021-08-11 19:13:52 +0530

    done

--- xamp/server.js
+++ xamp/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mysql = require("mysql");
-
+const bodyParser = require("body-parser");
 
 
 const db = mysql.createConnection({
@@ -20,7 +20,9 @@ db.connect((err)=>{
 });
 
 const app = express();
+app.use(bodyParser.json());
 
+//Creating databse
 app.get('/createDB',(req,res)=>{
     let sql = "CREATE DATABASE dbm";
     db.query(sql,(err)=>{
@@ -33,19 +35,8 @@ app.get('/createDB',(req,res)=>{
     })
 })
 
-app.get('/createDB',(req,res)=>{
-    let sql = "CREATE DATABASE dbm";
-    db.query(sql,(err)=>{
-        if(err){
-            console.log(err)
-        }
-        else {
-            res.send("DATABASE CREATED");
-        }
-    })
-})
 
-
+//Creating table
 app.get('/createtable',(req,res)=>{
     let sql = "CREATE TABLE students(id int AUTO_INCREMENT, name VARCHAR(200), PRIMARY KEY(id))"
     db.query(sql,(err)=>{
@@ -57,9 +48,10 @@ app.get('/createtable',(req,res)=>{
         }
     }) 
 })
-
+//Adding rows
 app.get('/add',(req,res)=>{
-    let add = {id:1,name:"IRFAN"};
+    // let add = {id:1,name:"IRFAN"};
+    let add = req.body;
     let sql = "INSERT INTO students SET ?";
     db.query(sql,add,(err)=>{
         if(err){
@@ -70,8 +62,9 @@ app.get('/add',(req,res)=>{
         }
     }) 
 })
-
+//Reading the rows
 app.get('/all',(req,res)=>{
+    console.log(req.body)
     let sql = "SELECT * FROM students";
     db.query(sql,(err,results)=>{
         if(err){
@@ -82,6 +75,22 @@ app.get('/all',(req,res)=>{
         }
     }) 
 })
+
+app.get('/student/:id',(req,res)=>{
+    
+    let sql = `SELECT * FROM students WHERE id =${req.params.id}`;
+    db.query(sql,(err,results)=>{
+        if(err){
+            console.log(err)
+        }
+        else {
+            res.send(results);
+        }
+    }) 
+})
+
+
+//Updating rows
 app.get('/update/:id',(req,res)=>{
    let update = "Updated Name";
    console.log(req.params.id)
@@ -95,7 +104,7 @@ app.get('/update/:id',(req,res)=>{
         }
     }) 
 })
-
+//Deleating rows
 app.get('/delete/:id',(req,res)=>{
     // console.log(req.params.id)
      let sql = `DELETE FROM students WHERE id =${req.params.id}`;
