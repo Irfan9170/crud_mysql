242e2f357c31420e8830254490a6e132b8522354
Irfan <irfanali987654@gmail.com>
2021-08-11 15:04:18 +0530

    started

---
+++ xamp/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mysql = require("mysql");
+
+
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:""
+});
+
+db.connect((err)=>{
+    if(err){
+        console.log(err)
+    }
+    else {
+        console.log("DB Connected")
+    }
+})
